6a574b72b887290aeae44fc6c5f4b08fafb67a53
Pavel Dubrova <pashadubrova@gmail.com>
2018-02-12 01:32:06 +0200

    input: touchscreen: cyttsp4/5: Global update
    Drivers are picked form the 26.3. Tulip kernel.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/input/touchscreen/cyttsp5/cyttsp5_i2c.c
+++ drivers/input/touchscreen/cyttsp5/cyttsp5_i2c.c
@@ -117,25 +117,23 @@ static struct of_device_id cyttsp5_i2c_of_match[] = {
 MODULE_DEVICE_TABLE(of, cyttsp5_i2c_of_match);
 #endif
 
-/* PERI-FG-TOUCH_CHECK_HW-00+[ */
 static int cyttsp5_ping_hw(struct device *dev)
 {
-    int rc, retry = 3;
-    char buf;
+	int rc, retry = 3;
+	char buf;
 
-    while (retry--)
-    {
-        rc = cyttsp5_i2c_read_default(dev, &buf, 1);
-        if (rc)
-            printk("%s: Read unsuccessful, try=%d\n", __func__, 3 - retry);
-        else
-            break;
-        msleep(100);
-    }
+	while (retry--)
+	{
+		rc = cyttsp5_i2c_read_default(dev, &buf, 1);
+		if (rc)
+			printk("%s: Read unsuccessful, try=%d\n", __func__, 3 - retry);
+		else
+			break;
+			msleep(100);
+	}
 
-    return rc;
+	return rc;
 }
-/* PERI-FG-TOUCH_CHECK_HW-00+] */
 
 static int cyttsp5_i2c_probe(struct i2c_client *client,
 	const struct i2c_device_id *i2c_id)
@@ -151,98 +149,11 @@ static int cyttsp5_i2c_probe(struct i2c_client *client,
 	dev_dbg(dev, "%s: debug on\n", __func__);
 	dev_vdbg(dev, "%s: verbose debug on\n", __func__);
 
-	{ // Enable power source
-
-		struct	power_source_data
-		{
-
-			char	*name;
-
-			unsigned	min_voltage, max_voltage, load_current;
-
-		};
-
-		struct power_source_data	power_source_DEFAULT[] =
-		{
-
-			{ "vdda", 2700000, 3300000, 15000 },
-			{ "vddio_DEFAULT", 1800000, 1800000, 15000 }
-
-		};
-
-		struct regulator	*power;
-
-		struct power_source_data	*power_source;
-
-		unsigned	loop, power_source_count;
-
-		power_source = power_source_DEFAULT;
-		power_source_count = sizeof( power_source_DEFAULT ) / sizeof( *power_source_DEFAULT );
-		printk( "ITUCH : Load default power source\n" );
-
-		for( loop = 0 ; loop < power_source_count ; ++loop )
-		{ // enable loop
-
-			struct power_source_data	*source = power_source + loop;
-
-			power	= regulator_get( dev, source->name );
-
-			if( IS_ERR( power ) )
-			{ // Failed
-
-				printk( "PTUCH : Failed to get %s regulator\n", source->name );
-
-				return	-1;
-
-			} // Failed
-
-			if( regulator_count_voltages( power ) > 0 )
-			{ // Set voltage & current
-
-				if( regulator_set_voltage( power, source->min_voltage, source->max_voltage ) )
-				{ // Failed
-
-					printk( "PTUCH : reg set %s vtg failed\n", source->name );
-
-					return	-1;
-
-				} // Failed
-
-				if( regulator_set_optimum_mode( power, source->load_current ) < 0 )
-				{ // Failed
-
-					printk( "PTUCH : Regulator %s set_opt failed\n", source->name );
-
-					return	-1;
-
-				} // Failed
-
-			} // Set voltage & current
-
-			if( regulator_enable( power ) )
-			{ // Failed
-
-				printk( "PTUCH : Regulator %s enable failed\n", source->name );
-
-				return	-1;
-
-			} // Failed
-
-		} // enable loop
-
-	} // Enable power source
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
 		dev_err(dev, "I2C functionality not Supported\n");
 		return -EIO;
 	}
 
-    /* PERI-FG-TOUCH_CHECK_HW-00+[ */
-    rc = cyttsp5_ping_hw(dev);
-    if (rc) {
-        dev_err(dev, "%s: No HW detected\n", __func__);
-        return rc;
-    }
-    /* PERI-FG-TOUCH_CHECK_HW-00+] */
 
 #ifdef CONFIG_TOUCHSCREEN_CYPRESS_CYTTSP5_DEVICETREE_SUPPORT
 	match = of_match_device(of_match_ptr(cyttsp5_i2c_of_match), dev);
@@ -252,6 +163,12 @@ static int cyttsp5_i2c_probe(struct i2c_client *client,
 			return rc;
 	}
 #endif
+
+	rc = cyttsp5_ping_hw(dev);
+	if (rc) {
+		dev_err(dev, "%s: No HW detected\n", __func__);
+		return rc;
+	}
 
 	rc = cyttsp5_probe(&cyttsp5_i2c_bus_ops, &client->dev, client->irq,
 			  CY_I2C_DATA_SIZE);
